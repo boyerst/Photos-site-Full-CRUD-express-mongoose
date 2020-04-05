c6675ed2b9af9791b359a12d55aac2725ad89b06
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-05 13:42:06 -0500

    photos controller working

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT
 // MIDDLEWARE
 app.use(express.static('public'))
 
+// CONTROLLERS
+const photoController = require('./controllers/photoController.js')
+// the following line means for all URLs starting with /authors, 
+// use the author controller, so 
+// note: Inside of author controller, the routes will NOT 
+// include '/authors' but they URLs will look the same to the client
+app.use('/photos', photoController)
+
+
 
 app.get('/', (req, res) => {
   res.render('home.ejs')
