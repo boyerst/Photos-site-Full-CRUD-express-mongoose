b35481de25f813ba370d71cd1067c6ff46103be4
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-05 15:15:53 -0500

    rendered registration page

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-// const User = require('../models/user')
+const User = require('../models/user')
 
 
 router.get('/register', (req, res) => {
-  res.send('Register controller working')
+  res.render('auth/register.ejs')
 })
 
 
