d1234c84c2d346b6c815c8df704e54b8700d2c04
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-07 19:04:26 -0500

    user required to post photos

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const Photo = require('../models/photo.js')
-const mongoose = require('mongoose')
+const User = require('../models/user')
 
 
 
 
-router.get('/', (req, res) => {
-  res.send('Photos controller working')
-})
+
+// router.get('/', (req, res) => {
+//   res.send('Photos controller working')
+// })
 
 
 
@@ -29,11 +30,42 @@ router.get('/new', (req, res) => {
  			user: req.session.userId
  		}
  		const createPhoto = await Photo.create(photoToCreate)
- 		res.send('check terminal')
+ 		res.redirect('/photos')
  	} catch(error) {
  		next(error)
  	}
  })
+
+//PHOTO INDEX ROUTE: GET /photos
+router.get('/', async (req, res, next) => {
+  try {
+    const foundPhotos = await Photo.find().populate('user')  
+    const session = req.session
+    console.log(foundPhotos);
+    res.render('photos/index.ejs', {
+      photos: foundPhotos, 
+      session: session
+    })
+  } catch(err) {
+    next(err)
+  }
+})
+
+
+//PHOTO SHOW ROUTE: GET /photos/:id
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const foundPhoto = await Photo.findById(req.params.id).populate('user')
+    res.render('photos/show.ejs', {
+      photo: foundPhoto,
+     
+    })
+  } catch (err) {
+  	next(err)
+  }
+})
+
 
 
 module.exports = router  
--- models/photo.js
+++ models/photo.js
@@ -20,6 +20,7 @@ const photoSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
+    required: true
   }
 })
 
