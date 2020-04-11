f0ecba5739556b83fad37a69f901b3417d12337f
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-11 13:45:15 -0500

    user can delete photos if logged in

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -21,50 +21,78 @@ router.get('/new', (req, res) => {
 //PHOTO CREATE ROUTE: POST /photos
  router.post('/', async (req, res, next) => {
  	try {
- 		console.log("here is the req.session in POST/photos")
- 		console.log(req.session)
- 		const photoToCreate = {
- 			title: req.body.title,
- 			url: req.body.url, 
- 			description: req.body.description, 
- 			user: req.session.userId
+ 		if(!req.session.loggedIn) {
+ 			req.session.message = "Go ahead and give us a register to do that!"
+ 			res.redirect('/auth/login')
+ 		} 
+ 		else {
+ 			console.log("here is the req.session in POST/photos")
+ 			console.log(req.session)
+ 			const photoToCreate = {
+ 				title: req.body.title,
+ 				url: req.body.url, 
+ 				description: req.body.description, 
+ 				user: req.session.userId
+ 			}
+ 			const createPhoto = await Photo.create(photoToCreate)
+ 			res.redirect('/photos')
  		}
- 		const createPhoto = await Photo.create(photoToCreate)
- 		res.redirect('/photos')
- 	} catch(error) {
- 		next(error)
+ 	} catch(err) {
+ 	  	next(err)
  	}
  })
+ 
 
 //PHOTO INDEX ROUTE: GET /photos
 router.get('/', async (req, res, next) => {
-  try {
-    const foundPhotos = await Photo.find().populate('user')  
-    const session = req.session
-    console.log(foundPhotos);
-    res.render('photos/index.ejs', {
-      photos: foundPhotos, 
-      session: session
-    })
-  } catch(err) {
-    next(err)
-  }
+  	try {
+    	const foundPhotos = await Photo.find().populate('user')  
+    	const session = req.session
+    	console.log(foundPhotos);
+    	res.render('photos/index.ejs', {
+      		photos: foundPhotos, 
+      		session: session
+    	})
+  	} catch(err) {
+    	next(err)
+  	}
 })
 
 
 //PHOTO SHOW ROUTE: GET /photos/:id
 
 router.get('/:id', async (req, res, next) => {
-  try {
-    const foundPhoto = await Photo.findById(req.params.id).populate('user')
-    res.render('photos/show.ejs', {
-      photo: foundPhoto,
-     
-    })
-  } catch (err) {
-  	next(err)
-  }
+  	try {
+    	const foundPhoto = await Photo.findById(req.params.id).populate('user')
+    	res.render('photos/show.ejs', {
+      		photo: foundPhoto
+    	})
+ 	 } catch (err) {
+  		next(err)
+  	}
 })
+
+
+//PHOTO DELETE ROUTE: DELETE /photos/:id
+
+router.delete('/:id', async (req, res, next) => {
+	try {
+		if(req.session.loggedIn == true) {
+			const deletedPhotos = await Photo.findByIdAndRemove(req.params.id).populate('user')
+			res.redirect('/photos')
+		} else {
+			// let message = res.locals.message
+			// res.locals.message = `Please log in before attemping this!`
+      		res.redirect('/photos')
+    	}
+	} catch(error) {
+		next(error)
+	}
+})
+
+
+
+
 
 
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const server = express()
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const session = require('express-session')
 const PORT = process.env.PORT
 
@@ -12,12 +13,32 @@ require('./db/db')
 server.use(express.static('public'))
 server.use(bodyParser.urlencoded({ extended: false }))
 
+server.use(methodOverride('_method'))
 // SESSIONS
 server.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false 
 }))
+
+
+
+server.use((req, res, next) => {
+
+
+  // adding these to res.locals allows us to make our templates
+  // more dynamic in the auth area
+  res.locals.loggedIn = req.session.loggedIn 
+  res.locals.username = req.session.username
+  // you may also want to store user id of logged in user!
+
+  // you can also use this to enhance your FLASH MESSAGING POWERS
+  res.locals.message = req.session.message
+  // as before, clear it out so it only appears once
+  req.session.message = undefined
+
+  next()
+})
 
 
 
