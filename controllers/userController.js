4ce321fea8118f84dccde1665f887e5cd2c875f5
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-12 21:03:35 -0500

    user can navigate to other user's show pages via the user index or througa link provided next to photographers photo in photo show page

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,44 +27,44 @@ router.get('/', async (req, res, next) => {
 
 //USER SHOW ROUTE: GET /users/:userId/photos
 
-// router.get('/:id/user', async (req, res, next) => {
-//   try {
-//     const foundUser = await User.findById(req.params.id)
-//     const foundPhotos  = await Photo.find( {user: req.params.id} ).populate('user')
-//     const username = req.session.username
-//     res.render('users/show.ejs', {
-//       user: foundUser,
-//       photo: foundPhotos,
-//       userId: req.session.userId,
-//       username: username
-//     })
-//   } catch (error) {
-//     next (error)
-//   }
-// })
-
-
-
-
 router.get('/:id/user', async (req, res, next) => {
   try {
     const foundUser = await User.findById(req.params.id)
     const foundPhotos  = await Photo.find( {user: req.params.id} ).populate('user')
-    // const foundPhotos  = await Photo.findById(req.params.id).populate('user')
     const username = req.session.username
-    const url = req.params.url
     res.render('users/show.ejs', {
       user: foundUser,
-      photos: foundPhotos,
+      photo: foundPhotos,
       userId: req.session.userId,
-      username: username,
-      // url: url
-
+      username: username
     })
   } catch (error) {
     next (error)
   }
 })
+
+
+
+
+// router.get('/:id/user', async (req, res, next) => {
+//   try {
+//     const foundUser = await User.findById(req.params.id)
+//     const foundPhotos  = await Photo.find( {user: req.params.id} ).populate('user')
+//     // const foundPhotos  = await Photo.findById(req.params.id).populate('user')
+//     const username = req.session.username
+//     const url = req.params.url
+//     res.render('users/show.ejs', {
+//       user: foundUser,
+//       photos: foundPhotos,
+//       userId: req.session.userId,
+//       username: username,
+//       // url: url
+
+//     })
+//   } catch (error) {
+//     next (error)
+//   }
+// })
 
 
 
